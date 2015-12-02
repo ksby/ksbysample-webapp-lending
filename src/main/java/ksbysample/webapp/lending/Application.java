574a9220d3aa8950fa134791f881b0939583293a
ksby <kusa@db3.so-net.ne.jp>
2015-12-03 00:08:29 +0900

    #46 JPA の AutoConfiguration を無効にしました。

--- src/main/java/ksbysample/webapp/lending/Application.java
+++ src/main/java/ksbysample/webapp/lending/Application.java
@@ -3,16 +3,17 @@ package ksbysample.webapp.lending;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-import java.io.Serializable;
 import java.text.MessageFormat;
 
 @ImportResource("classpath:applicationContext-${spring.profiles.active}.xml")
-@SpringBootApplication
+@SpringBootApplication(exclude = {JpaRepositoriesAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @ComponentScan("ksbysample")
 @EnableRedisHttpSession
 @EnableGlobalMethodSecurity(prePostEnabled = true)
