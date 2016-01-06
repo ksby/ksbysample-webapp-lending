39ca30b8399b27b1aae73b523779a18e66b697e2
ksby <kusa@db3.so-net.ne.jp>
2016-01-06 19:59:27 +0900

    #51 TestDataResource にテストデータロード機能を追加し、TestDataLoaderResource クラスはテストデータをロードするユーティリティクラス TestDataLoader へ変更しました。

--- src/test/java/ksbysample/common/test/TestDataLoader.java
+++ src/test/java/ksbysample/common/test/TestDataLoader.java
@@ -1,12 +1,40 @@
 package ksbysample.common.test;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.ReplacementDataSet;
+import org.dbunit.dataset.csv.CsvDataSet;
+import org.dbunit.operation.DatabaseOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-public @interface TestDataLoader {
-        String value();
+import javax.sql.DataSource;
+import java.io.File;
+
+@Component
+public class TestDataLoader {
+    
+    @Autowired
+    private DataSource dataSource;
+
+    public void load(String csvDir) {
+        try {
+            IDatabaseConnection conn = null;
+            try {
+                conn = new DatabaseConnection(dataSource.getConnection());
+
+                IDataSet dataSet = new CsvDataSet(new File(csvDir));
+                ReplacementDataSet replacementDataset = new ReplacementDataSet(dataSet);
+                replacementDataset.addReplacementObject("[null]", null);
+                DatabaseOperation.CLEAN_INSERT.execute(conn, replacementDataset);
+            } finally {
+                if (conn != null) conn.close();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/test/java/ksbysample/common/test/TestDataResource.java
+++ src/test/java/ksbysample/common/test/TestDataResource.java
@@ -3,27 +3,28 @@ package ksbysample.common.test;
 import org.dbunit.database.DatabaseConnection;
 import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.database.QueryDataSet;
-import org.dbunit.dataset.DefaultDataSet;
-import org.dbunit.dataset.DefaultTable;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.ReplacementDataSet;
 import org.dbunit.dataset.csv.CsvDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.dbunit.operation.DatabaseOperation;
-import org.junit.rules.ExternalResource;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.lang.annotation.Annotation;
 import java.nio.file.Files;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 @Component
-public class TestDataResource extends ExternalResource {
+public class TestDataResource extends TestWatcher {
 
     private final String TESTDATA_DIR = "src/test/resources/testdata/base";
     private final String BACKUP_FILE_NAME = "ksbylending_backup";
@@ -38,38 +39,55 @@ public class TestDataResource extends ExternalResource {
     @Autowired
     private DataSource dataSource;
 
+    @Autowired
+    private TestDataLoader testDataLoader;
+    
     private File backupFile;
     
     @Override
-    protected void before() throws Exception {
+    protected void starting(Description description) {
         IDatabaseConnection conn = null;
         try {
-            conn = new DatabaseConnection(dataSource.getConnection());
+            try {
+                conn = new DatabaseConnection(dataSource.getConnection());
 
-            // バックアップを取得する
-            QueryDataSet partialDataSet = new QueryDataSet(conn);
-            for (String backupTable : BACKUP_TABLES) {
-                partialDataSet.addTable(backupTable);
-            }
-            ReplacementDataSet replacementDatasetBackup = new ReplacementDataSet(partialDataSet);
-            replacementDatasetBackup.addReplacementObject("", "[null]");
-            backupFile = File.createTempFile(BACKUP_FILE_NAME, "xml");
-            try (FileOutputStream fos = new FileOutputStream(backupFile)) {
-                FlatXmlDataSet.write(replacementDatasetBackup, fos);
-            }
+                // バックアップを取得する
+                QueryDataSet partialDataSet = new QueryDataSet(conn);
+                for (String backupTable : BACKUP_TABLES) {
+                    partialDataSet.addTable(backupTable);
+                }
+                ReplacementDataSet replacementDatasetBackup = new ReplacementDataSet(partialDataSet);
+                replacementDatasetBackup.addReplacementObject("", "[null]");
+                backupFile = File.createTempFile(BACKUP_FILE_NAME, "xml");
+                try (FileOutputStream fos = new FileOutputStream(backupFile)) {
+                    FlatXmlDataSet.write(replacementDatasetBackup, fos);
+                }
 
-            // テストデータに入れ替える
-            IDataSet dataSet = new CsvDataSet(new File(TESTDATA_DIR));
-            ReplacementDataSet replacementDataset = new ReplacementDataSet(dataSet);
-            replacementDataset.addReplacementObject("[null]", null);
-            DatabaseOperation.CLEAN_INSERT.execute(conn, replacementDataset);
-        } finally {
-            if (conn != null) conn.close();
+                // テストデータに入れ替える
+                IDataSet dataSet = new CsvDataSet(new File(TESTDATA_DIR));
+                ReplacementDataSet replacementDataset = new ReplacementDataSet(dataSet);
+                replacementDataset.addReplacementObject("[null]", null);
+                DatabaseOperation.CLEAN_INSERT.execute(conn, replacementDataset);
+
+                // テストメソッドに @TestData アノテーションが付加されている場合には、
+                // アノテーションで指定されたテストデータをロードする
+                Collection<Annotation> annotationList = description.getAnnotations();
+                for (Annotation annotation : annotationList) {
+                    if (annotation instanceof TestData) {
+                        TestData testData = (TestData)annotation;
+                        testDataLoader.load(testData.value());
+                    }
+                }
+            } finally {
+                if (conn != null) conn.close();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
     @Override
-    protected void after() {
+    protected void finished(Description description) {
         try {
             IDatabaseConnection conn = null;
             try {
@@ -93,7 +111,7 @@ public class TestDataResource extends ExternalResource {
                 }
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 
