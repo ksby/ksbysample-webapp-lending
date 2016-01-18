4d575cfd954c29eaceeb7999a2613e592d362be1
ksby <kusa@db3.so-net.ne.jp>
2016-01-19 00:58:03 +0900

    #53 承認権限 ( ROLE_APPROVER ) を付与されていないユーザが /lendingapproval にアクセスした場合にはエラーになるようにしました。

--- src/main/java/ksbysample/webapp/lending/web/lendingapproval/LendingapprovalController.java
+++ src/main/java/ksbysample/webapp/lending/web/lendingapproval/LendingapprovalController.java
@@ -5,6 +5,7 @@ import ksbysample.webapp.lending.helper.message.MessagesPropertiesHelper;
 import ksbysample.webapp.lending.helper.thymeleaf.SuccessMessagesHelper;
 import org.seasar.doma.jdbc.OptimisticLockException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.mail.MessagingException;
 
 @Controller
+@PreAuthorize("hasRole('ROLE_APPROVER')")
 @RequestMapping("/lendingapproval")
 public class LendingapprovalController {
 
