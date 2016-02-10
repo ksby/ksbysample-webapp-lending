981e17d996dbc54aabfd244d21cae8e92895e749
ksby <kusa@db3.so-net.ne.jp>
2016-02-11 00:29:00 +0900

    #81 build タスク実行時に無検査キャストの警告が出たので警告が出たクラスを修正しました

--- src/main/java/ksbysample/webapp/lending/view/BookListCsvView.java
+++ src/main/java/ksbysample/webapp/lending/view/BookListCsvView.java
@@ -18,6 +18,7 @@ public class BookListCsvView extends AbstractView {
     private static final String[] CSV_HEADER = new String[]{"ISBN", "書名", "申請理由", "承認／却下", "却下理由"};
     private static final String CSV_FILE_NAME_FORMAT = "booklist-%s.csv";
 
+    @SuppressWarnings("unchecked")
     @Override
     protected void renderMergedOutputModel(Map<String, Object> model
             , HttpServletRequest request, HttpServletResponse response) throws Exception {
