cc5c2f161725360f51c4f2817e5a4599c6cc7f26
ksby <kusa@db3.so-net.ne.jp>
2016-02-13 19:56:26 +0900

    #88 TestDataResource クラスでバックアップ＆ロード＆リストアするテストデータを @BaseTestData アノテーションで指定できるようにしました

--- src/test/java/ksbysample/common/test/rule/db/TestDataResource.java
+++ src/test/java/ksbysample/common/test/rule/db/TestDataResource.java
@@ -12,6 +12,7 @@ import org.dbunit.operation.DatabaseOperation;
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
@@ -19,6 +20,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -48,11 +50,14 @@ public class TestDataResource extends TestWatcher {
             if (!hasNoUseTestDataResourceAnnotation(description)) {
                 conn = DbUnitUtils.createDatabaseConnection(dataSource);
 
-                // バックアップを取得する
-                backupDb(conn);
+                // バックアップ＆ロード＆リストア対象のテストデータのパスを取得する
+                String testDataBaseDir = getBaseTestDir(description);
 
-                // TESTDATA_BASE_DIR で指定されたディレクトリ内のテストデータをロードする
-                testDataLoader.load(TESTDATA_BASE_DIR);
+                // バックアップを取得する
+                backupDb(conn, testDataBaseDir);
+
+                // テストデータをロードする
+                testDataLoader.load(testDataBaseDir);
 
                 // テストメソッドに @TestData アノテーションが付加されている場合には、
                 // アノテーションで指定されたテストデータをロードする
@@ -63,7 +68,8 @@ public class TestDataResource extends TestWatcher {
         } finally {
             try {
                 if (conn != null) conn.close();
-            } catch (Exception ignored) {}
+            } catch (Exception ignored) {
+            }
         }
     }
 
@@ -83,7 +89,8 @@ public class TestDataResource extends TestWatcher {
         } finally {
             try {
                 if (conn != null) conn.close();
-            } catch (Exception ignored) {}
+            } catch (Exception ignored) {
+            }
 
             if (backupFile != null) {
                 try {
@@ -103,14 +110,45 @@ public class TestDataResource extends TestWatcher {
         return result;
     }
 
-    private void backupDb(IDatabaseConnection conn)
+    private String getBaseTestDir(Description description) {
+        // @BaseTestData アノテーションで指定されている場合にはそれを使用し、指定されていない場合には
+        // TESTDATA_BASE_DIR 定数で指定されているものと使用する
+
+        // テストメソッドに @BaseTestData アノテーションが付加されているかチェックする
+        BaseTestData baseTestData = description.getAnnotation(BaseTestData.class);
+        if (baseTestData != null) {
+            return baseTestData.value();
+        }
+
+        // TestDataResource クラスのフィールドに @BaseTestData アノテーションが付加されているかチェックする
+        Field[] fields = description.getTestClass().getDeclaredFields();
+        for (Field field : fields) {
+            if (field.getType().equals(TestDataResource.class)) {
+                baseTestData = field.getAnnotation(BaseTestData.class);
+                if (baseTestData != null) {
+                    return baseTestData.value();
+                }
+            }
+        }
+
+        // テストクラスに @BaseTestData アノテーションが付加されているかチェックする
+        Class<?> testClass = description.getTestClass();
+        baseTestData = AnnotationUtils.findAnnotation(testClass, BaseTestData.class);
+        if (baseTestData != null) {
+            return baseTestData.value();
+        }
+
+        return TESTDATA_BASE_DIR;
+    }
+
+    private void backupDb(IDatabaseConnection conn, String testDataBaseDir)
             throws DataSetException, IOException {
         QueryDataSet partialDataSet = new QueryDataSet(conn);
 
         // TESTDATA_BASE_DIR で指定されたディレクトリ内の table-ordering.txt に記述されたテーブル名一覧を取得し、
         // バックアップテーブルとしてセットする
-        List<String> backupTableList = Files.readAllLines(Paths.get(TESTDATA_BASE_DIR, "table-ordering.txt"));
-        for (String backupTable :  backupTableList) {
+        List<String> backupTableList = Files.readAllLines(Paths.get(testDataBaseDir, "table-ordering.txt"));
+        for (String backupTable : backupTableList) {
             partialDataSet.addTable(backupTable);
         }
 
@@ -136,7 +174,7 @@ public class TestDataResource extends TestWatcher {
         description.getAnnotations().stream()
                 .filter(annotation -> annotation instanceof TestData)
                 .forEach(annotation -> {
-                    TestData testData = (TestData)annotation;
+                    TestData testData = (TestData) annotation;
                     testDataLoader.load(testData.value());
                 });
     }
