75cad6e9083384d8b9d98a4d5b5d6a9cd48461c8
ksby <kusa@db3.so-net.ne.jp>
2017-04-02 00:46:44 +0900

    #128 LibraryHelperTest クラスと InquiringStatusOfBookQueueListenerTest クラスのモック化の処理を JMockit → @MockBean + Mockito へ変更しました

--- src/test/java/ksbysample/webapp/lending/helper/library/LibraryHelperTest.java
+++ src/test/java/ksbysample/webapp/lending/helper/library/LibraryHelperTest.java
@@ -2,33 +2,29 @@ package ksbysample.webapp.lending.helper.library;
 
 import ksbysample.webapp.lending.dao.LibraryForsearchDao;
 import ksbysample.webapp.lending.entity.LibraryForsearch;
-import mockit.Delegate;
-import mockit.Expectations;
-import mockit.Injectable;
-import mockit.Tested;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LibraryHelperTest {
 
-    @Tested
+    @Autowired
     private LibraryHelper libraryHelper;
 
-    @Injectable
+    @MockBean
     private LibraryForsearchDao libraryForsearchDao;
 
     @Test
     public void testGetSelectedLibrary_図書館が選択されていない場合() throws Exception {
-        new Expectations() {{
-            libraryForsearchDao.selectSelectedLibrary();
-            result = null;
-        }};
+        given(libraryForsearchDao.selectSelectedLibrary()).willReturn(null);
 
         String result = libraryHelper.getSelectedLibrary();
         assertThat(result).isEqualTo("※図書館が選択されていません");
@@ -36,17 +32,10 @@ public class LibraryHelperTest {
 
     @Test
     public void testGetSelectedLibrary_図書館が選択されている場合() throws Exception {
-        new Expectations() {{
-            libraryForsearchDao.selectSelectedLibrary();
-            result = new Delegate<LibraryForsearch>() {
-                LibraryForsearch aDelegateMethod() {
-                    LibraryForsearch libraryForsearch = new LibraryForsearch();
-                    libraryForsearch.setSystemid("System_Id");
-                    libraryForsearch.setFormal("図書館名");
-                    return libraryForsearch;
-                }
-            };
-        }};
+        LibraryForsearch libraryForsearch = new LibraryForsearch();
+        libraryForsearch.setSystemid("System_Id");
+        libraryForsearch.setFormal("図書館名");
+        given(libraryForsearchDao.selectSelectedLibrary()).willReturn(libraryForsearch);
 
         String result = libraryHelper.getSelectedLibrary();
         assertThat(result).isEqualTo("選択中：図書館名");
--- src/test/java/ksbysample/webapp/lending/listener/rabbitmq/InquiringStatusOfBookQueueListenerTest.java
+++ src/test/java/ksbysample/webapp/lending/listener/rabbitmq/InquiringStatusOfBookQueueListenerTest.java
@@ -12,8 +12,6 @@ import ksbysample.webapp.lending.service.calilapi.response.Book;
 import ksbysample.webapp.lending.service.calilapi.response.Libkey;
 import ksbysample.webapp.lending.service.calilapi.response.SystemData;
 import ksbysample.webapp.lending.service.queue.InquiringStatusOfBookQueueMessage;
-import mockit.Mock;
-import mockit.MockUp;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.csv.CsvDataSet;
 import org.junit.Rule;
@@ -24,6 +22,7 @@ import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.mail.internet.MimeMessage;
@@ -33,9 +32,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static mockit.Deencapsulation.getField;
-import static mockit.Deencapsulation.setField;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -58,77 +57,60 @@ public class InquiringStatusOfBookQueueListenerTest {
     @Autowired
     private InquiringStatusOfBookQueueListener listener;
 
+    @MockBean
+    private LibraryForsearchDao libraryForsearchDao;
+
+    @MockBean
+    private CalilApiService calilApiService;
+
     @Test
     @TestData("listener/rabbitmq/testdata/001")
     public void testReceiveMessage() throws Exception {
-        // モックに入れ替える前のフィールドの実体を退避する
-        LibraryForsearchDao libraryForsearchDaoOrg = getField(listener, "libraryForsearchDao");
-        CalilApiService calilApiServiceOrg = getField(listener, "calilApiService");
-        try {
-            /**
-             * モック定義部
-             */
-            // InquiringStatusOfBookQueueListener.libraryForsearchDao をモックに入れ替える
-            LibraryForsearchDao libraryForsearchDao = new MockUp<LibraryForsearchDao>() {
-                @Mock
-                LibraryForsearch selectSelectedLibrary() {
-                    LibraryForsearch libraryForsearch = new LibraryForsearch();
-                    libraryForsearch.setSystemid("System_Id");
-                    libraryForsearch.setFormal("図書館名");
-                    return libraryForsearch;
-                }
-            }.getMockInstance();
-            setField(listener, "libraryForsearchDao", libraryForsearchDao);
+        /**
+         * モック定義部
+         */
+        LibraryForsearch libraryForsearch = new LibraryForsearch();
+        libraryForsearch.setSystemid("System_Id");
+        libraryForsearch.setFormal("図書館名");
+        given(libraryForsearchDao.selectSelectedLibrary()).willReturn(libraryForsearch);
 
-            // InquiringStatusOfBookQueueListener.calilApiService をモックに入れ替える
-            CalilApiService calilApiService = new MockUp<CalilApiService>() {
-                @Mock
-                public List<Book> check(String systemid, List<String> isbnList) {
-                    List<Book> bookList = new ArrayList<>();
-                    bookList.add(new Book("978-4-7741-6366-6", null, new SystemData(null, null, null, Arrays.asList(new Libkey(null, "貸出可")))));
-                    bookList.add(new Book("978-4-7741-5377-3", null, new SystemData(null, null, null, Arrays.asList(new Libkey(null, "蔵書あり")))));
-                    bookList.add(new Book("978-4-7973-8014-9", null, new SystemData(null, null, null, Arrays.asList(new Libkey(null, "貸出中")))));
-                    bookList.add(new Book("978-4-7973-4778-4", null, new SystemData(null, null, null, Arrays.asList(new Libkey(null, "準備中")))));
-                    bookList.add(new Book("978-4-87311-704-1", null, new SystemData(null, null, null, Arrays.asList(new Libkey(null, "蔵書なし")))));
-                    return bookList;
-                }
-            }.getMockInstance();
-            setField(listener, "calilApiService", calilApiService);
+        List<Book> bookList = new ArrayList<>();
+        bookList.add(new Book("978-4-7741-6366-6", null, new SystemData(null, null, null, Arrays.asList(new Libkey(null, "貸出可")))));
+        bookList.add(new Book("978-4-7741-5377-3", null, new SystemData(null, null, null, Arrays.asList(new Libkey(null, "蔵書あり")))));
+        bookList.add(new Book("978-4-7973-8014-9", null, new SystemData(null, null, null, Arrays.asList(new Libkey(null, "貸出中")))));
+        bookList.add(new Book("978-4-7973-4778-4", null, new SystemData(null, null, null, Arrays.asList(new Libkey(null, "準備中")))));
+        bookList.add(new Book("978-4-87311-704-1", null, new SystemData(null, null, null, Arrays.asList(new Libkey(null, "蔵書なし")))));
+        given(calilApiService.check(any(), any())).willReturn(bookList);
 
-            /**
-             * テスト本体
-             */
-            InquiringStatusOfBookQueueMessage queueMessage = new InquiringStatusOfBookQueueMessage();
-            queueMessage.setLendingAppId(1L);
-            Message message = messageConverter.toMessage(queueMessage, new MessageProperties());
-            listener.receiveMessage(message);
+        /**
+         * テスト本体
+         */
+        InquiringStatusOfBookQueueMessage queueMessage = new InquiringStatusOfBookQueueMessage();
+        queueMessage.setLendingAppId(1L);
+        Message message = messageConverter.toMessage(queueMessage, new MessageProperties());
+        listener.receiveMessage(message);
 
-            /**
-             * 検証
-             */
-            // テーブルの以下のカラムのデータを検証する
-            //  ・lending_app.status
-            //  ・lending_book.lending_state
-            IDataSet dataSet = new CsvDataSet(new File("src/test/resources/ksbysample/webapp/lending/listener/rabbitmq/assertdata/001"));
-            TableDataAssert tableDataAssert = new TableDataAssert(dataSet, dataSource);
-            tableDataAssert.assertEquals("lending_app", new String[]{"lending_app_id", "lending_user_id", "approval_user_id", "version"});
-            tableDataAssert.assertEquals("lending_book", new String[]{"lending_book_id", "lending_app_id", "isbn", "book_name", "lending_app_flg", "lending_app_reason", "approval_result", "approval_reason", "version"});
+        /**
+         * 検証
+         */
+        // テーブルの以下のカラムのデータを検証する
+        //  ・lending_app.status
+        //  ・lending_book.lending_state
+        IDataSet dataSet = new CsvDataSet(new File("src/test/resources/ksbysample/webapp/lending/listener/rabbitmq/assertdata/001"));
+        TableDataAssert tableDataAssert = new TableDataAssert(dataSet, dataSource);
+        tableDataAssert.assertEquals("lending_app", new String[]{"lending_app_id", "lending_user_id", "approval_user_id", "version"});
+        tableDataAssert.assertEquals("lending_book", new String[]{"lending_book_id", "lending_app_id", "isbn", "book_name", "lending_app_flg", "lending_app_reason", "approval_result", "approval_reason", "version"});
 
-            // 送信されたメールを検証する
-            assertThat(mailServer.getMessagesCount()).isEqualTo(1);
-            MimeMessage mimeMessage = mailServer.getFirstMessage();
-            assertThat(mimeMessage.getRecipients(javax.mail.Message.RecipientType.TO))
-                    .extracting(Object::toString)
-                    .containsOnly("tanaka.taro@sample.com");
-            assertThat(mimeMessage.getContent())
-                    .isEqualTo(com.google.common.io.Files.toString(
-                            new File("src/test/resources/ksbysample/webapp/lending/helper/mail/assertdata/001/message.txt")
-                            , Charsets.UTF_8));
-        } finally {
-            // モックに差し替えたフィールドを退避しておいた元の実体に戻す
-            setField(listener, "libraryForsearchDao", libraryForsearchDaoOrg);
-            setField(listener, "calilApiService", calilApiServiceOrg);
-        }
+        // 送信されたメールを検証する
+        assertThat(mailServer.getMessagesCount()).isEqualTo(1);
+        MimeMessage mimeMessage = mailServer.getFirstMessage();
+        assertThat(mimeMessage.getRecipients(javax.mail.Message.RecipientType.TO))
+                .extracting(Object::toString)
+                .containsOnly("tanaka.taro@sample.com");
+        assertThat(mimeMessage.getContent())
+                .isEqualTo(com.google.common.io.Files.toString(
+                        new File("src/test/resources/ksbysample/webapp/lending/helper/mail/assertdata/001/message.txt")
+                        , Charsets.UTF_8));
     }
 
 }
