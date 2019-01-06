7988f55d2e5f00b50b584ed67b06c600f492e382
ksby <kusa@db3.so-net.ne.jp>
2019-01-06 14:02:11 +0000

    #132 Redis のクライアントライブラリを Jedis → Lettuce に変更しました

--- src/main/java/ksbysample/webapp/lending/config/RedisClusterConfig.java
+++ src/main/java/ksbysample/webapp/lending/config/RedisClusterConfig.java
@@ -1,13 +1,17 @@
 package ksbysample.webapp.lending.config;
 
+import io.lettuce.core.cluster.ClusterClientOptions;
+import io.lettuce.core.cluster.ClusterTopologyRefreshOptions;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
+import java.time.Duration;
 import java.util.List;
 
 /**
@@ -21,13 +25,29 @@ public class RedisClusterConfig {
     List<String> nodes;
 
     /**
-     * Redis Cluster に接続するための {@link JedisConnectionFactory} オブジェクトを生成する
+     * Redis Cluster に接続するための {@link LettuceConnectionFactory} オブジェクトを生成する
      *
-     * @return {@link JedisConnectionFactory} オブジェクト
+     * @return {@link LettuceConnectionFactory} オブジェクト
      */
     @Bean
     public RedisConnectionFactory connectionFactory() {
-        return new JedisConnectionFactory(new RedisClusterConfiguration(nodes));
+        ClusterTopologyRefreshOptions clusterTopologyRefreshOptions = ClusterTopologyRefreshOptions.builder()
+                .enablePeriodicRefresh(Duration.ofSeconds(30))
+                .enableAllAdaptiveRefreshTriggers()
+                .build();
+        ClusterClientOptions clusterClientOptions = ClusterClientOptions.builder()
+                .validateClusterNodeMembership(false)
+                .topologyRefreshOptions(clusterTopologyRefreshOptions)
+                .build();
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .commandTimeout(Duration.ofSeconds(15))
+                .shutdownTimeout(Duration.ZERO)
+                .clientOptions(clusterClientOptions)
+                .build();
+
+        RedisClusterConfiguration serverConfig = new RedisClusterConfiguration(nodes);
+
+        return new LettuceConnectionFactory(serverConfig, clientConfig);
     }
 
 }
