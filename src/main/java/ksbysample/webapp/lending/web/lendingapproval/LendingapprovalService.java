880a47fa5a8b4fba30a279acfc828f1aaa01bf58
ksby <kusa@db3.so-net.ne.jp>
2019-03-21 12:24:13 +0000

    #133 コメントアウトしていた error-prone を復活させてバージョンを 2.3.1 → 2.3.3 へ上げました。net.ltgt.errorprone プラグインのバージョンも 0.0.16 → 0.7.1 へ上げました。

--- src/main/java/ksbysample/webapp/lending/web/lendingapproval/LendingapprovalService.java
+++ src/main/java/ksbysample/webapp/lending/web/lendingapproval/LendingapprovalService.java
@@ -87,7 +87,7 @@ public class LendingapprovalService {
      * @param lendingapprovalForm ???
      * @throws MessagingException
      */
-    @SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
+    @SuppressWarnings({"PMD.AvoidInstantiatingObjectsInLoops", "UnusedVariable"})
     public void complete(LendingapprovalForm lendingapprovalForm) throws MessagingException {
         // 更新対象のデータを取得する(ロックする)
         Long lendingAppId = lendingapprovalForm.getLendingApp().getLendingAppId();
